1418bb5fd08e24977dd5170560ffcbb609b4a741
zplato <zach.plato@gmail.com>
2024-03-27 20:45:33 -0400

    adding initial backend repo

---
+++ backend/src/app.py
@@ -0,0 +1,19 @@
+import random
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def process_data():
+    respirations = str(random.randint(10, 50))
+    print("HELLO WORLD")
+    return "Respiratory Rate: {0}".format(respirations)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
