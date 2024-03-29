7993ba735a6b3ee6686204c0775e0552c886645b
Brian Daley <brian@uconn.edu>
2024-03-29 18:49:49 -0400

    Ability to delete pets

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -11,10 +11,20 @@
           <v-card-text v-if="pets.length === 0">
             No pets found. Add a pet to get started.
           </v-card-text>
-          <v-list :items="pets" item-props lines="three">
-            <template v-slot:subtitle="{ subtitle }">
-              <div v-html="subtitle"></div>
-            </template>
+          <v-list>
+
+            <v-list-item v-for="pet in pets" :key="pet.id" :subtitle="pet.subtitle"
+              :title="pet.title" :to="pet.to" class="mb-3">
+              <template v-slot:prepend>
+                <v-avatar color="grey-lighten-1">
+                  <v-img :src="pet.prependAvatar"></v-img>
+                </v-avatar>
+              </template>
+
+              <template v-slot:append>
+                <v-btn color="grey-lighten-1" icon="mdi-delete" variant="text" @click.prevent="deletePet(pet.id)"></v-btn>
+              </template>
+            </v-list-item>
           </v-list>
         </v-card>
       </v-col>
@@ -25,9 +35,6 @@
       </v-col>
     </v-row>
   </v-container>
-  <!-- <v-navigation-drawer v-model="drawer" location="bottom" temporary>
-    <v-list :items="pets"></v-list>
-  </v-navigation-drawer> -->
 </template>
 
 <script>
@@ -38,18 +45,30 @@ export default {
     drawer: false,
     pets: [],
   }),
-  async created() {
-    let pets = await db.pets.toArray()
-    pets.forEach(pet => {
-      this.pets.push( {
-        title: pet.name || 'Unnamed Pet',
-        subtitle: `${pet.type || 'Unspecified Type'} / ${pet.breed || 'Unspecified Breed'}`,
-        prependAvatar: pet.photo ? URL.createObjectURL(pet.photo) : null,
-        to: `/pet/${pet.id}`,
-      
+  methods: {
+    async deletePet(id, e) {
+      if(confirm('Are you sure you want to delete this pet?')) {
+        await db.pets.delete(id);
+        this.pets = this.pets.filter(p => p.id !== id);
+      }
+      return false
+    },
+    async getPets() {
+      this.pets = [];
+      let pets = await db.pets.toArray();
+      pets.forEach(pet => {
+        this.pets.push({
+          id: pet.id,
+          title: pet.name || 'Unnamed Pet',
+          subtitle: `${pet.type || 'Unspecified Type'} / ${pet.breed || 'Unspecified Breed'}`,
+          prependAvatar: pet.photo ? URL.createObjectURL(pet.photo) : null,
+          to: `/pet/${pet.id}`,
+        });
       });
-      
-    });
+    },
+  },
+  async created() {
+    await this.getPets();
   },
 }
 </script>
